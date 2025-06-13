c761c564746d6b450bcaf9e7c7bbf7846419516e
Sanal Tserenov <sanaltserenov@outlook.com>
2025-06-13 20:43:14 +0300

    add login & signup pages

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,49 @@
 import Handlebars from "handlebars/runtime";
+import { identity, isEmpty } from "./utils/mydash";
 import "./style.css";
 
 import { chatsList } from "./data";
 
 // partials
 import chatListItem from "./partials/chat-list-item.hbs";
+import inputField from "./partials/input-field.hbs";
+import modalBox from "./partials/modal-box.hbs";
+import button from "./partials/button.hbs";
+import centeredBox from "./partials/centered-box.hbs";
 
 // templates
+import login from "./templates/login.hbs";
+import signup from "./templates/signup.hbs";
 import chats from "./templates/chats.hbs";
 
 Handlebars.registerPartial("chatListItem", chatListItem);
+Handlebars.registerPartial("inputField", inputField);
+Handlebars.registerPartial("modalBox", modalBox);
+Handlebars.registerPartial("button", button);
+Handlebars.registerPartial("centeredBox", centeredBox);
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.querySelector<HTMLDivElement>("#app");
-  const result = chats({ chatsList });
+  let result: string;
+
+  const route = document.location.pathname
+    .split("?")[0]
+    .split("/")
+    .filter(identity)[0];
+
+  if (isEmpty(route)) {
+    result = chats({ chatsList });
+  } else if (route === "login") {
+    result = login({});
+  } else if (route === "signup") {
+    result = signup({});
+  } else if (route === "profile") {
+    result = "profile";
+  } else if (route === "500") {
+    result = "500";
+  } else {
+    result = "404";
+  }
 
   root!.innerHTML = result;
 });
