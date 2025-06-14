befd1186446d0468b49adf3c7ec98f3d756b8164
Sanal Tserenov <sanaltserenov@outlook.com>
2025-06-14 20:40:18 +0300

    add profile edit and change password screens

--- src/main.ts
+++ src/main.ts
@@ -10,27 +10,39 @@ import inputField from "./partials/input-field.hbs";
 import modalBox from "./partials/modal-box.hbs";
 import button from "./partials/button.hbs";
 import centeredBox from "./partials/centered-box.hbs";
+import avatar from "./partials/avatar.hbs";
+import labeledValue from "./partials/labeled-value.hbs";
+import labeledInput from "./partials/labeled-input.hbs";
+import profileLayout from "./layout/profile-layout.hbs";
 
 // templates
 import login from "./templates/login.hbs";
 import signup from "./templates/signup.hbs";
 import chats from "./templates/chats.hbs";
+import profile from "./templates/profile.hbs";
+import profileEdit from "./templates/profile-edit.hbs";
+import changePassword from "./templates/change-password.hbs";
 
 Handlebars.registerPartial("chatListItem", chatListItem);
 Handlebars.registerPartial("inputField", inputField);
 Handlebars.registerPartial("modalBox", modalBox);
 Handlebars.registerPartial("button", button);
 Handlebars.registerPartial("centeredBox", centeredBox);
+Handlebars.registerPartial("avatar", avatar);
+Handlebars.registerPartial("labeledValue", labeledValue);
+Handlebars.registerPartial("labeledInput", labeledInput);
+Handlebars.registerPartial("profileLayout", profileLayout);
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.querySelector<HTMLDivElement>("#app");
   let result: string;
 
-  const route = document.location.pathname
+  const [route, subroute] = document.location.pathname
     .split("?")[0]
     .split("/")
-    .filter(identity)[0];
+    .filter(identity);
 
+  // temporary routing
   if (isEmpty(route)) {
     result = chats({ chatsList });
   } else if (route === "login") {
@@ -38,7 +50,15 @@ document.addEventListener("DOMContentLoaded", () => {
   } else if (route === "signup") {
     result = signup({});
   } else if (route === "profile") {
-    result = "profile";
+    if (isEmpty(subroute)) {
+      result = profile({});
+    } else if (subroute === "edit") {
+      result = profileEdit({});
+    } else if (subroute === "change-password") {
+      result = changePassword({});
+    } else {
+      result = "404";
+    }
   } else if (route === "500") {
     result = "500";
   } else {
