2cb8557111b774aa68ba6666184f87e9203c214f
Sanal Tserenov <sanaltserenov@outlook.com>
2025-06-14 21:09:58 +0300

    add server error screens

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ import centeredBox from "./partials/centered-box.hbs";
 import avatar from "./partials/avatar.hbs";
 import labeledValue from "./partials/labeled-value.hbs";
 import labeledInput from "./partials/labeled-input.hbs";
+import errorView from "./partials/error-view.hbs";
 import profileLayout from "./layout/profile-layout.hbs";
 
 // templates
@@ -22,6 +23,8 @@ import chats from "./templates/chats.hbs";
 import profile from "./templates/profile.hbs";
 import profileEdit from "./templates/profile-edit.hbs";
 import changePassword from "./templates/change-password.hbs";
+import notFound from "./templates/not-found.hbs";
+import serverError from "./templates/server-error.hbs";
 
 Handlebars.registerPartial("chatListItem", chatListItem);
 Handlebars.registerPartial("inputField", inputField);
@@ -32,6 +35,7 @@ Handlebars.registerPartial("avatar", avatar);
 Handlebars.registerPartial("labeledValue", labeledValue);
 Handlebars.registerPartial("labeledInput", labeledInput);
 Handlebars.registerPartial("profileLayout", profileLayout);
+Handlebars.registerPartial("errorView", errorView);
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.querySelector<HTMLDivElement>("#app");
@@ -57,12 +61,12 @@ document.addEventListener("DOMContentLoaded", () => {
     } else if (subroute === "change-password") {
       result = changePassword({});
     } else {
-      result = "404";
+      result = notFound({});
     }
   } else if (route === "500") {
-    result = "500";
+    result = serverError({});
   } else {
-    result = "404";
+    result = notFound({});
   }
 
   root!.innerHTML = result;
